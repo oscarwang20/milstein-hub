b763a66d355c606e4d68c9774d17c96eb83235f5
Oscar Wang <oscarwang22396@gmail.com>
2024-01-21 11:44:29 -0500

    change image url

--- frontend/src/components/Hello.tsx
+++ frontend/src/components/Hello.tsx
@@ -9,7 +9,12 @@ const Hello = () => {
     <div>
       This is an api generated message:{" "}
       <span className="text-blue-500">{hello}</span>
-      <Image src="/austin.jpg" alt="austin" width={500} height={500} />
+      <Image
+        src="https://images.squarespace-cdn.com/content/v1/553d091be4b0c687eb3f1b4c/1430066929023-9TLC6IF5QAUAVFG48VA6/image-asset.jpeg?format=750w"
+        alt="austin"
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
