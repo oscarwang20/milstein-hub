e05c701a9dc05b604bd7079fbeaf501b1f3a0d7b
Oscar Wang <oscarwang22396@gmail.com>
2024-01-21 12:04:06 -0500

    tried again

--- frontend/src/components/Hello.tsx
+++ frontend/src/components/Hello.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { getHello } from "../api/api";
+import Austin from "../assets/austin.jpg";
 
 const Hello = () => {
   const hello = getHello();
@@ -9,12 +10,7 @@ const Hello = () => {
     <div>
       This is an api generated message:{" "}
       <span className="text-blue-500">{hello}</span>
-      <Image
-        src="https://images.squarespace-cdn.com/content/v1/553d091be4b0c687eb3f1b4c/1430066929023-9TLC6IF5QAUAVFG48VA6/image-asset.jpeg?format=750w"
-        alt="austin"
-        width={500}
-        height={500}
-      />
+      <Image src={Austin} alt="austin" width={500} height={500} />
     </div>
   );
 };
